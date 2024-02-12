13a05f39b6796367ddf6c615fd4c25be447acc78
Eshwar <eshwarbandi18@gmail.com>
2024-02-12 05:08:58 +0000

    commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const {Storage} = require('@google-cloud/storage');
 const csv = require('csv-parser');
 
 exports.readObservation = (file, context) => {
-    // console.log(`  Event: ${context.eventId}`);
-    // console.log(`  Event Type: ${context.eventType}`);
-    // console.log(`  Bucket: ${file.bucket}`);
-    // console.log(`  File: ${file.name}`);
+     console.log(`  Event: ${context.eventId}`);
+     console.log(`  Event Type: ${context.eventType}`);
+     console.log(`  Bucket: ${file.bucket}`);
+     console.log(`  File: ${file.name}`);
 
     const gcs = new Storage();
 
@@ -36,3 +36,32 @@ function printDict(row) {
         console.log(`${key} : ${row[key]}`);
     }
 }
+
+function convertNegative9999ToNull(value) {
+    // Convert -9999 to null
+    return (value === -9999) ? null : value;
+  }
+  
+  function convertToDecimal(value) {
+    // Convert to decimal by dividing by 10
+    return (value !== null) ? value / 10 : null;
+  }
+  
+  function transformNumericFields(obj) {
+    const numericFields = ['year', 'month', 'day', 'hour', 'winddirection', 'sky', 'airtemp', 'dewpoint', 'pressure', 'windspeed', 'precip1hour', 'precip6hour'];
+  
+    for (let key of numericFields) {
+      if (obj.hasOwnProperty(key)) {
+        if (key === 'station') {
+          // Special case for 'station' field
+          obj[key] = (obj[key] === '724380-93819') ? obj[key] : null;
+        } else {
+          // Common case for other numeric fields
+          obj[key] = convertNegative9999ToNull(obj[key]);
+          obj[key] = convertToDecimal(obj[key]);
+        }
+      }
+    }
+  
+    return obj;
+  }
